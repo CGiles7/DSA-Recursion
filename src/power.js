f39f3900178e3b39ae277257fb65b47a1e3a61c8
Callie Giles <anchoredinthecrown@gmail.com>
2023-09-20 11:14:41 -0400

    complete recursive function

--- src/power.js
+++ src/power.js
@@ -5,7 +5,13 @@
  * @param {integer} exponent a non-negative integer
  */
 function power(base, exponent) {
- 
+    if (exponent < 0) {
+     throw error = "exponent should be >= 0";
+   } else if (exponent === 0) {
+     return 1;
+   } else {
+     return base * power(base, exponent - 1);
+   }
 }
 
 module.exports = power;
